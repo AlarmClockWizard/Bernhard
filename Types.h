4a5226cdf6a40efea382b2c75d836c9f4cdc61ee
Roland Gruber <roland@Desktoprechner-Keller.fritz.box>
2019-09-29 17:38:49 +0200

    added playback "loop"

---
+++ Types.h
@@ -0,0 +1,37 @@
+/**
+ * This file is part of AwesomeArduinoAlarmClock.
+ * 
+ * AwesomeArduinoAlarmClock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * AwesomeArduinoAlarmClock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with AwesomeArduinoAlarmClock.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright © 2011-2012 Roland
+ **/
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <Arduino.h>
+
+typedef char            int8;
+typedef unsigned char   uint8;
+typedef int16_t         int16;
+typedef uint16_t        uint16;
+typedef int32_t         int32;
+typedef unsigned int    uint32;
+typedef int64_t         int64;
+typedef uint64_t        uint64;
+typedef float           float32;
+typedef double          float64;
+
+#endif //TYPES_H
+
